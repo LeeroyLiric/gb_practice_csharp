68fcb6c4c28566d5fe81b1eee6020a53ebcb4abf
Korolev_Kirill <tk.kirg14@gmail.com>
2023-02-24 18:50:30 +0300

    Solved third task

--- Seminar_Third_dir/s3_task_2_class.cs
+++ Seminar_Third_dir/s3_task_2_class.cs
@@ -11,6 +11,36 @@ public static class s3_task_2_class
 {
     public static void s3_SecondTaskSolution()
     {
+        double parsed;
+        List<double> coordinates = new List<double>();
+        Console.WriteLine("Введите координаты точек в одной строке по маске:\nA (x,y,z); B (x1,y1,z1);");
+        var inputs = Console.ReadLine();
+        if (inputs != null)
+        {
+            string[] splittedInput = inputs.Split(new[] { ',', '(', ')' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            foreach ( var input in splittedInput ) 
+            {
+                if (double.TryParse(input, out parsed)) coordinates.Add(parsed);
+            }
+
+            if (coordinates.Count() != 6)
+            {
+                Console.WriteLine("Вы ввели некорректные числа! Запустите задачу заново и попробуйте ещё раз\n");
+                return;
+            }
+            
+            double distance = Math.Sqrt(
+                Math.Pow(coordinates[0] - coordinates[3], 2) + // x - x1
+                Math.Pow(coordinates[1] - coordinates[4], 2) + // y - y1
+                Math.Pow(coordinates[2] - coordinates[5], 2)); // z - z1
+
+            Console.WriteLine(Math.Round(distance, 2).ToString());
+        }
+        else
+        {
+            Console.WriteLine("Вы не ввели числа! Запустите задачу заново и попробуйте ещё раз\n");
+        }
 
     }
 }
