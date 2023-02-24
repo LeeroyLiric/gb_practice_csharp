80337a2e70e41a9f4ff9afe5c17211392394c786
Korolev_Kirill <tk.kirg14@gmail.com>
2023-02-24 18:06:10 +0300

    Solved second task in third seminar

--- Seminar_Third_dir/s3_task_3_class.cs
+++ Seminar_Third_dir/s3_task_3_class.cs
@@ -7,10 +7,19 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+using static PromptClass;
+
 public static class s3_task_3_class
 {
     public static void s3_ThirdTaskSolution()
     {
+        double exponent = 3;
+        int n = Prompt("Введите ваше число:");
+        for (int i = 1; i <= n; i++)
+        {
+            string result = Math.Pow(i, exponent).ToString() + (i == n ? "\n" : ", ");
+            Console.Write(result);
+        }
         
     }
 }
