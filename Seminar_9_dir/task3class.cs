9d1d54300c823430b2d24dc61c1b108825f3e4a2
Korolev_Kirill <tk.kirg14@gmail.com>
2023-03-17 20:07:40 +0300

    Solved Sem 9 task 3 hw

--- Seminar_9_dir/task3class.cs
+++ Seminar_9_dir/task3class.cs
@@ -14,7 +14,25 @@ namespace gb_practice_csharp.Seminar_9_dir
         /// </summary>
         public static void Solution()
         {
-
+            long m = Convert.ToInt64(PromptClass.Prompt("M = "));
+            long n = Convert.ToInt64(PromptClass.Prompt("N = "));
+            long result = Ackermann(m, n);
+            System.Console.WriteLine(result.ToString());
         }
+
+        static long Ackermann(long m, long n) 
+        {
+            while (m != 0L)
+            {
+                if (n == 0L)
+                    n = 1L;
+                else
+                    n = Ackermann(m, n - 1L);
+                m--;
+            }
+            return n + 1L;
+        }
+        
+        
     }
 }
