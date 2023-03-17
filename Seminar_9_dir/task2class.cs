670081710cd28e14c841f033a0093f1930d69658
Korolev_Kirill <tk.kirg14@gmail.com>
2023-03-17 19:10:34 +0300

    Solved Sem 9 task 2 hw

--- Seminar_9_dir/task2class.cs
+++ Seminar_9_dir/task2class.cs
@@ -8,15 +8,28 @@ namespace gb_practice_csharp.Seminar_9_dir
     /// M = 4; N = 8 -> 30
     /// </summary>
     public static class TaskSecondClass
-    {   
+    {
         /// <summary>
         /// Решение задача 2 семинар 9
         /// </summary>
         public static void Solution()
         {
-
+            int m = PromptClass.Prompt("M = ");
+            int n = PromptClass.Prompt("N = ");
+            int summ = RecursionSum(m, n);
+            Console.WriteLine(summ);
         }
 
-        
+        static int RecursionSum(int m, int n)
+        {
+            int summ = m;
+
+            if (m < n)
+            {
+               summ += RecursionSum(m + 1, n);
+            }
+
+            return summ;
+        }
     }
 }
